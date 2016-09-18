2731757d6999b32ee3105a90c43e964bcad72fcb
Geir Gulbrandsen <dobbelg@gmail.com>
2016-09-18 17:48:14 +0100

    Add support for currency

--- src/com/plusonetesting/TDDBE/Franc.java
+++ src/com/plusonetesting/TDDBE/Franc.java
@@ -2,12 +2,15 @@ package com.plusonetesting.TDDBE;
 
 public class Franc extends Money{
 
-    public Franc(int amount) {
-        this.amount = amount;
+    public Franc(int amount, String currency) {
+        super(amount, currency);
     }
 
     public Money times(int multiplier) {
-        return new Franc(amount * multiplier);
+        return Money.franc(amount * multiplier);
     }
 
+    String currency() {
+        return currency;
+    }
 }
--- src/com/plusonetesting/TDDBE/Money.java
+++ src/com/plusonetesting/TDDBE/Money.java
@@ -3,6 +3,12 @@ package com.plusonetesting.TDDBE;
 abstract class Money {
 
     protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     public boolean equals(Object object) {
         Money money = (Money) object;
@@ -10,13 +16,13 @@ abstract class Money {
                 && getClass().equals(money.getClass());
     }
 
-    static Money dollar(int amount) {
-        return new Dollar(amount);
-    }
+    static Money dollar(int amount) { return new Dollar(amount, "USD"); }
 
-    static Money franc(int amount) {
-        return new Franc(amount);
-    }
+    static Money franc(int amount) { return new Franc(amount, "CHF"); }
 
-    abstract Money times(int multipler);
+    abstract Money times(int multiplier);
+
+    String currency() {
+        return currency;
+    };
 }
